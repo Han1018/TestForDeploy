17170ad630a20772ef02502504e197294508627e
Han1018 <t107590040@ntut.org.tw>
2020-07-27 18:51:19 +0800

    no message

---
+++ dist/can-script.2d4973ed.js
@@ -0,0 +1,519 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"can-script.js":[function(require,module,exports) {
+// create a variable to store the products 'database' in
+var products; // use fetch to retrieve it, and report any errors that occur in the fetch operation
+// once the products have been successfully loaded and formatted as a JSON object
+// using response.json(), run the initialize() function
+
+fetch('products.json').then(function (response) {
+  if (response.ok) {
+    response.json().then(function (json) {
+      products = json;
+      initialize();
+    });
+  } else {
+    console.log('Network request for products.json failed with response ' + response.status + ': ' + response.statusText);
+  }
+}); // sets up the app logic, declares required variables, contains all the other functions
+
+function initialize() {
+  // grab the UI elements that we need to manipulate
+  var category = document.querySelector('#category');
+  var searchTerm = document.querySelector('#searchTerm');
+  var searchBtn = document.querySelector('button');
+  var main = document.querySelector('main'); // keep a record of what the last category and search term entered were
+
+  var lastCategory = category.value; // no search has been made yet
+
+  var lastSearch = ''; // these contain the results of filtering by category, and search term
+  // finalGroup will contain the products that need to be displayed after
+  // the searching has been done. Each will be an array containing objects.
+  // Each object will represent a product
+
+  var categoryGroup;
+  var finalGroup; // To start with, set finalGroup to equal the entire products database
+  // then run updateDisplay(), so ALL products are displayed initially.
+
+  finalGroup = products;
+  updateDisplay(); // Set both to equal an empty array, in time for searches to be run
+
+  categoryGroup = [];
+  finalGroup = []; // when the search button is clicked, invoke selectCategory() to start
+  // a search running to select the category of products we want to display
+
+  searchBtn.onclick = selectCategory;
+
+  function selectCategory(e) {
+    // Use preventDefault() to stop the form submitting — that would ruin
+    // the experience
+    e.preventDefault(); // Set these back to empty arrays, to clear out the previous search
+
+    categoryGroup = [];
+    finalGroup = []; // if the category and search term are the same as they were the last time a
+    // search was run, the results will be the same, so there is no point running
+    // it again — just return out of the function
+
+    if (category.value === lastCategory && searchTerm.value.trim() === lastSearch) {
+      return;
+    } else {
+      // update the record of last category and search term
+      lastCategory = category.value;
+      lastSearch = searchTerm.value.trim(); // In this case we want to select all products, then filter them by the search
+      // term, so we just set categoryGroup to the entire JSON object, then run selectProducts()
+
+      if (category.value === 'All') {
+        categoryGroup = products;
+        selectProducts(); // If a specific category is chosen, we need to filter out the products not in that
+        // category, then put the remaining products inside categoryGroup, before running
+        // selectProducts()
+      } else {
+        // the values in the <option> elements are uppercase, whereas the categories
+        // store in the JSON (under "type") are lowercase. We therefore need to convert
+        // to lower case before we do a comparison
+        var lowerCaseType = category.value.toLowerCase();
+
+        for (var i = 0; i < products.length; i++) {
+          // If a product's type property is the same as the chosen category, we want to
+          // dispay it, so we push it onto the categoryGroup array
+          if (products[i].type === lowerCaseType) {
+            categoryGroup.push(products[i]);
+          }
+        } // Run selectProducts() after the filtering has bene done
+
+
+        selectProducts();
+      }
+    }
+  } // selectProducts() Takes the group of products selected by selectCategory(), and further
+  // filters them by the tnered search term (if one has bene entered)
+
+
+  function selectProducts() {
+    // If no search term has been entered, just make the finalGroup array equal to the categoryGroup
+    // array — we don't want to filter the products further — then run updateDisplay().
+    if (searchTerm.value.trim() === '') {
+      finalGroup = categoryGroup;
+      updateDisplay();
+    } else {
+      // Make sure the search term is converted to lower case before comparison. We've kept the
+      // product names all lower case to keep things simple
+      var lowerCaseSearchTerm = searchTerm.value.trim().toLowerCase(); // For each product in categoryGroup, see if the search term is contained inside the product name
+      // (if the indexOf() result doesn't return -1, it means it is) — if it is, then push the product
+      // onto the finalGroup array
+
+      for (var i = 0; i < categoryGroup.length; i++) {
+        if (categoryGroup[i].name.indexOf(lowerCaseSearchTerm) !== -1) {
+          finalGroup.push(categoryGroup[i]);
+        }
+      } // run updateDisplay() after this second round of filtering has been done
+
+
+      updateDisplay();
+    }
+  } // start the process of updating the display with the new set of products
+
+
+  function updateDisplay() {
+    // remove the previous contents of the <main> element
+    while (main.firstChild) {
+      main.removeChild(main.firstChild);
+    } // if no products match the search term, display a "No results to display" message
+
+
+    if (finalGroup.length === 0) {
+      var para = document.createElement('p');
+      para.textContent = 'No results to display!';
+      main.appendChild(para); // for each product we want to display, pass its product object to fetchBlob()
+    } else {
+      for (var i = 0; i < finalGroup.length; i++) {
+        fetchBlob(finalGroup[i]);
+      }
+    }
+  } // fetchBlob uses fetch to retrieve the image for that product, and then sends the
+  // resulting image display URL and product object on to showProduct() to finally
+  // display it
+
+
+  function fetchBlob(product) {
+    // construct the URL path to the image file from the product.image property
+    var url = 'images/' + product.image;
+    console.log(url); // Use fetch to fetch the image, and convert the resulting response to a blob
+    // Again, if any errors occur we report them in the console.
+
+    fetch(url).then(function (response) {
+      if (response.ok) {
+        response.blob().then(function (blob) {
+          // Convert the blob to an object URL — this is basically an temporary internal URL
+          // that points to an object stored inside the browser
+          var objectURL = URL.createObjectURL(blob);
+          console.log(objectURL); // invoke showProduct
+
+          showProduct(objectURL, product);
+        });
+      } else {
+        console.log('Network request for "' + product.name + '" image failed with response ' + response.status + ': ' + response.statusText);
+      }
+    });
+  } // Display a product inside the <main> element
+
+
+  function showProduct(objectURL, product) {
+    // create <section>, <h2>, <p>, and <img> elements
+    var section = document.createElement('section');
+    var heading = document.createElement('h2');
+    var para = document.createElement('p');
+    var image = document.createElement('img'); // give the <section> a classname equal to the product "type" property so it will display the correct icon
+
+    section.setAttribute('class', product.type); // Give the <h2> textContent equal to the product "name" property, but with the first character
+    // replaced with the uppercase version of the first character
+
+    heading.textContent = product.name.replace(product.name.charAt(0), product.name.charAt(0).toUpperCase()); // Give the <p> textContent equal to the product "price" property, with a $ sign in front
+    // toFixed(2) is used to fix the price at 2 decimal places, so for example 1.40 is displayed
+    // as 1.40, not 1.4.
+
+    para.textContent = '$' + product.price.toFixed(2); // Set the src of the <img> element to the ObjectURL, and the alt to the product "name" property
+
+    image.src = objectURL;
+    image.alt = product.name;
+
+    try {
+      image.onload = function () {
+        window.URL.revokeObjectURL(image.src);
+        console.log("revoke");
+      };
+    } catch (e) {
+      console.log("ERROR", e);
+    } // append the elements to the DOM as appropriate, to add the product to the UI
+
+
+    main.appendChild(section);
+    section.appendChild(heading);
+    section.appendChild(para);
+    section.appendChild(image);
+    console.log("has load here");
+  }
+}
+},{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63643" + '/');
+
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      }); // Enable HMR for CSS by default.
+
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID; // html encode message and stack trace
+
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+
+  if (checkedAssets[id]) {
+    return;
+  }
+
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+
+    return true;
+  }
+}
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","can-script.js"], null)
+//# sourceMappingURL=/can-script.2d4973ed.js.map
